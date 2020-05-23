9590385122f5da6e82779cc9e4f8f8e61a98ffb9
Mischa Saunders <mischa.saunders@ackama.com>
2020-05-24 00:55:47 +1200

    begin layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,37 +1,82 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import useSiteMetadata from "./SiteMetadata";
-
+import useSiteMetadata from "./SiteMetadata"
+// import { Helmet } from "react-helmet";
+// import Footer from "../components/Footer";
+// import Navbar from "../components/Navbar";
+// import { withPrefix } from "gatsby";
+// import { ThemeProvider, ColorModeProvider, CSSReset } from "@chakra-ui/core";
+// import theme from "../theme/theme";
+// import styled from "@emotion/styled";
 import Header from "./header"
-import "./layout.css"
+import styled from "@emotion/styled"
+
+const PageWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  min-height: 100vh;
+`
+const ContenteWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 800px;
+  min-width: 350px;
+  width: 100%;
+  margin: 1rem;
+`
+
+const LeftBar = styled.div`
+  min-height: 100%;
+  width: 5rem;
+  background-color: #b38b00;
+`
+
+const RightBar = styled.div`
+  min-height: 100%;
+  width: 5rem;
+  background-color: #1a1b1f;
+`
+
+const TopBar = styled.div`
+  height: 5rem;
+  background-color: #688bf6;
+  width: 50%;
+`
+const TopBar2 = styled.div`
+  height: 5rem;
+  background-color: #5da13c;
+  width: 50%;
+`
+
+const Flex = styled.div`
+  display: flex;
+`
 
 const Layout = ({ children }) => {
-  const { title, description } = useSiteMetadata();
+  const { title, description } = useSiteMetadata()
+  // #688bf6
+  // #5da13c
+  // #1A1B1F
+  // #b38b00
 
   return (
     <>
-      <Header siteTitle={title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <Flex>
+        <TopBar />
+        <TopBar2 />
+      </Flex>
+      <main>
+        <PageWrapper>
+          <LeftBar />
+          <ContenteWrapper>{children}</ContenteWrapper>
+          <RightBar />
+        </PageWrapper>
+      </main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
     </>
   )
 }
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,34 +1,42 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-import { Box, Text } from "@chakra-ui/core";
+import { Box, Text } from "@chakra-ui/core"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import { Button } from "@chakra-ui/core"
+import { Heading } from "@chakra-ui/core"
+import { Flex } from "@chakra-ui/core"
+import BlogRoll from "../components/BlogRoll"
 
-export const IndexPageTemplate = ({title}) => (
+export const IndexPageTemplate = ({ title }) => (
   <>
     <SEO title="Home" />
-    <Box p={8}>
+    <Heading as="h4" size="lg" fontWeight={500} textAlign={"center"} my={5}>
       <Text fontSize="xl">{title}</Text>
-    </Box>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    </Heading>
+    <BlogRoll />
+    <Flex justify={"center"}>
+      <Link to="/blog">
+        <Button size="lg" variant="outline">
+          Read More
+        </Button>
+      </Link>
+    </Flex>
   </>
 )
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark;
+  const { frontmatter } = data.markdownRemark
 
   return (
     <Layout>
       <IndexPageTemplate title={frontmatter.title} />
     </Layout>
-  );
-};
+  )
+}
 
-export default IndexPage;
+export default IndexPage
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
@@ -38,5 +46,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
-
+`
